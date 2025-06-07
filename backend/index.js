164439d89113da38002f47b221ad0783cf6000de
AshaPatel216 <asha@promactinfo.com>
2025-06-07 14:12:50 +0530

    connection made between backend and db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const pool = require('./db'); // database connection
 
 app.use(express.json()); // to parse the request body
 app.use(cors()); // to allow requests from different origins
